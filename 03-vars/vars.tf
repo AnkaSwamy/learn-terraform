41a7e90933a00ad4926909178af2719a7538a279
Anka swamy <ankaswamy.telagorla@gmail.com>
2023-07-18 22:07:34 +0530

    variable declaration and data types

--- 03-vars/vars.tf
+++ 03-vars/vars.tf
@@ -16,3 +16,41 @@ value= var.sample1
 output "sample-ext" {
 value= "value of sample-ext - ${var.sample-ext}"
 }
+
+
+# plain
+variable "course" {
+default = "Devops training"
+}
+#List
+variable "courses" {
+default= [
+"DevOps"
+"Aws"
+"python"
+]
+}
+#Map
+variable "Course_details" {
+default= {
+DevOps= {
+name= "Devops"
+timings= "7AM"
+duration= "90days"
+}
+AWS= {
+name = "aws"
+time="8pm"
+duration=99
+}
+}
+}
+output "course" {
+value= var.course
+}
+output "course" {
+value= var.courses
+}
+output "Course_details" {
+value= var.Courses_details
+}
