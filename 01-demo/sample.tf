a8ed02740f51ef4028bf2dd1cd81735809184f1b
Anka swamy <ankaswamy.telagorla@gmail.com>
2023-07-18 18:31:12 +0530

    Terraform sample file

--- 01-demo/sample.tf
+++ 01-demo/sample.tf
@@ -1,8 +1,16 @@
-resource "aws_instance" {
-  ami  = "ami-03265a0778a880afb"
-  instance_type          = "t3.micro"
-  tags = {
-    name   = "Helloworld"
+module "ec2_instance" {
+  source  = "terraform-aws-modules/ec2-instance/aws"
 
+  name = "single-instance"
+
+  instance_type          = "t2.micro"
+  key_name               = "user1"
+  monitoring             = true
+  vpc_security_group_ids = ["sg-12345678"]
+  subnet_id              = "subnet-eddcdzz4"
+
+  tags = {
+    Terraform   = "true"
+    Environment = "dev"
   }
 }
