fbc51214661ef9078e462038263596a8030c9d83
Anka swamy <ankaswamy.telagorla@gmail.com>
2023-07-18 21:42:57 +0530

    Terraform sample file

--- 01-demo/sample.tf
+++ 01-demo/sample.tf
@@ -1,8 +1,9 @@
 resource  "aws_instance" "web" {
-  ami                    = "ami-03265a0778a880afb"
-  instance_type          = "t3.micro"
+  ami             = "ami-03265a0778a880afb"
+  instance_type    = "t3.micro"
   tags = {
-    name   = "Hello-world"
+    name   = "createdinstance"
 
   }
 }
+
