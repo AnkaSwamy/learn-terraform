18e134229991d9e24f5b2b836ea29bd7d5c96a43
Anka swamy <ankaswamy.telagorla@gmail.com>
2023-07-19 16:21:10 +0530

    create sec grp and connect with multiple instances and printing ip address

--- module-multiple/ec2/main.tf
+++ module-multiple/ec2/main.tf
@@ -41,7 +41,3 @@ resource "aws_security_group" "sg" {
 }
 
 variable "name" {}
-
-output "public_ip" {
-  value= aws_instance.web.public_ip
-}
---
+++ module-multiple/main.tf
@@ -0,0 +1,9 @@
+module "instance1" {
+  source= "./ec2"
+  name = "instance1"
+
+}
+module "instance2" {
+  source = "./ec2"
+  name   = "instance2"
+}
