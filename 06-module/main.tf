2ae21319c58b54d4ee73129c73a3c4815781707c
Anka swamy <ankaswamy.telagorla@gmail.com>
2023-09-15 18:34:07 +0530

    security groups needs to be connected to craeted instance

--- 06-module/main.tf
+++ 06-module/main.tf
@@ -1,8 +1,3 @@
 module "sample1" {
-  source= "./ec2"
-
-}
-module "sample2" {
-  source= "./ec2"
-
+  source = "./ec2"
 }
--- module-multiple/main.tf
+++
@@ -1,9 +0,0 @@
-module "instance1" {
-  source= "./ec2"
-  name = "instance1"
-
-}
-module "instance2" {
-  source = "./ec2"
-  name   = "instance2"
-}
