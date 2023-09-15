d8c93fa0a9262861158547017b7ad55bcd537e0a
Anka swamy <ankaswamy.telagorla@gmail.com>
2023-09-15 22:34:15 +0530

    provisioner

--- 08-provisioner/main.tf
+++ 08-provisioner/main.tf
@@ -1,26 +1,30 @@
-resource "aws_instance" "web" {
-  ami           = data.aws_ami.example.id
-  instance_type = "t3.micro"
-  vpc_security_group_ids = ["sg-0abba694acb6b6339"]
+ resource "aws_instance" "web" {
+   ami           = data.aws_ami.example.id
+   instance_type = "t3.micro"
+   vpc_security_group_ids = ["sg-0abba694acb6b6339"]
 
-  tags = {
-    Name = "Pro"
-  }
-  provisioner "remote-exec" {
-  connection {
-    type     = "ssh"
-    user     = "centos"
-    password = "DevOps321"
-    host     = self.public_ip
-  }
+   tags = {
+     Name = "Pro"
+   }
+
+   provisioner "remote-exec" {
+
+     connection {
+     type     = "ssh"
+     user     = "centos"
+     password = "DevOps321"
+     host     = self.public_ip
+   }
 
     inline = [
       "sudo labauto ansible",
       "ansible-pull -i localhost, -u https://github.com/AnkaSwamy/roboshop-ansible.git main.yml -e env=dev -e role_name=frontend"
     ]
   }
-}
-data "aws_ami" "example" {
+
+ }
+
+ data "aws_ami" "example" {
   owners           = ["973714476881"]
   most_recent      = true
   name_regex       = "Centos-8-DevOps-Practice"
