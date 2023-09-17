766039585a10d0ba419de96f147a10a2726f5047
Anka swamy <ankaswamy.telagorla@gmail.com>
2023-09-17 12:59:23 +0530

    using for -each loop function to create multiple instances

---
+++ for-each/main.tf
@@ -0,0 +1,31 @@
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.example.id
+  for_each = var.instances
+  instance_type = lookup(each.value,"instance_type","t3.small")
+
+  tags = {
+    Name =each.key
+  }
+}
+
+data "aws_ami" "example" {
+  owners           = ["973714476881"]
+  most_recent      = true
+  name_regex       = "Centos-8-DevOps-Practice"
+}
+variable "instances"{
+  default= {
+    frontend={
+      name=frontend
+
+    }
+    catalogue={
+      name=catalogue
+      instance_type="t3.nano"
+    }
+    cart={
+      name=cart
+      instance_type="t3.nano"
+    }
+  }
+  }
