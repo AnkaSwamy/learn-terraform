615dc900dd9cfd2d0530132db50846314121d56d
Anka swamy <ankaswamy.telagorla@gmail.com>
2023-09-23 12:02:16 +0530

    checking catalogue service is working or not

--- 09-roboshop-v1/ec2/main.tf
+++ 09-roboshop-v1/ec2/main.tf
@@ -1,56 +1,67 @@
 resource "aws_instance" "web" {
-  ami           = data.aws_ami.example.id
-  instance_type = "t3.small"
+  ami                    = data.aws_ami.example.id
+  instance_type          = "t3.small"
   vpc_security_group_ids = [aws_security_group.sg.id]
 
   tags = {
     Name = "var.name"
   }
-  provisioner "remote-exec" {
+  resource "null_resource" "ansible" {
+    depends_on = [aws_instances.web, aws_route53_record.www]
+    provisioner "remote-exec" {
 
-    connection {
-      type     = "ssh"
-      user     = "centos"
-      password = "DevOps321"
-      host     = self.public_ip
+      connection {
+        type     = "ssh"
+        user     = "centos"
+        password = "DevOps321"
+        host     = self.public_ip
+      }
+
+      inline = [
+        "sudo labauto ansible",
+        "ansible-pull -i localhost, -U https://github.com/ankadevopsb73/roboshop-ansible main.yml -e env=dev -e role_name=${var.name}"
+      ]
     }
 
-    inline = [
-      "sudo labauto ansible",
-      "ansible-pull -i localhost, -U https://github.com/ankadevopsb73/roboshop-ansible main.yml -e env=dev -e role_name=${var.name}"
-    ]
+
+  }
+  resource "aws_route53_record" "www" {
+    zone_id = "Z045686737G7Y72P7F78S"
+    name    = "${var.name}-dev"
+    type    = "A"
+    ttl     = 30
+    records = [aws_instance.web.public_ip]
   }
 
 
-}
-
-data "aws_ami" "example" {
-  owners           = ["973714476881"]
-  most_recent      = true
-  name_regex       = "Centos-8-DevOps-Practice"
-}
-
-resource "aws_security_group" "sg" {
-  name        = "var.name"
-  description = "Allow TLS inbound traffic"
-
-
-  ingress {
-    description      = "ssh"
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
+  data "aws_ami" "example" {
+    owners      = ["973714476881"]
+    most_recent = true
+    name_regex  = "Centos-8-DevOps-Practice"
   }
 
-  egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-  }
+  resource "aws_security_group" "sg" {
+    name        = "var.name"
+    description = "Allow TLS inbound traffic"
 
-  tags = {
-    Name = "var.name"
+
+    ingress {
+      description = "ssh"
+      from_port   = 0
+      to_port     = 0
+      protocol    = "-1"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    egress {
+      from_port   = 0
+      to_port     = 0
+      protocol    = "-1"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    tags = {
+      Name = "var.name"
+    }
   }
 }
