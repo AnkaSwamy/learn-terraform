7388fd64ee6f6311f0bef67067e973bbf1e8909c
Anka swamy <ankaswamy.telagorla@gmail.com>
2023-10-04 15:54:44 +0530

    checking provisioners

--- 09-roboshop-v1/main.tf
+++ 09-roboshop-v1/main.tf
@@ -1,19 +1,22 @@
-module "frontend" {
-  source = "./ec2"
-  name = "frontend"
+module "instances" {
+  for_each = var.instances
+  source   = "./ec2"
+  name     = each.key
 }
-variable "instances"{
-  default={
-    frontend= {}
-    mongodb= {}
-    catalogue= {}
-    redis= {}
-    cart= {}
-    user= {}
-    mysql= {}
-    shipping= {}
-    rabbitmq = {}
-    payment= {}
+
+variable "instances" {
+  default = {
+
+    frontend  = {}
+    mongodb   = {}
+    catalogue = {}
+    redis     = {}
+    user      = {}
+    cart      = {}
+    mysql     = {}
+    shipping  = {}
+    rabbitmq  = {}
+    payment   = {}
 
   }
 }
